16f93005feb08d8192b14fad1b51056295b99aa8
ARUN C <edizocorp@gmail.com>
2025-05-26 11:10:25 +0530

    fix bug 1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Main-Webpage">
       <App />
     </BrowserRouter>
   </StrictMode>
