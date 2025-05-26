e328a053b8001fdf681c7eb3af6fd6edb5bc9157
ARUN C <edizocorp@gmail.com>
2025-05-26 10:02:36 +0530

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Main-Webpage/',
+  base: '/Main-Webpage/', // Replace with your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
